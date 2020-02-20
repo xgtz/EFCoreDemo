b7e8ce03672344b8d822879d20952b04f6cf4161
xgtz <526042790@qq.com>
2020-02-20 23:15:16 +0800

    添加项目文件。

---
+++ EFCoreDemo/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreDemo.Models
+{
+    public class DataContext:DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
